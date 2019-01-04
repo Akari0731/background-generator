1efeb20429e03214835ee2d1189400072a424c6a
Akari Nishii <nishiiakari0731@gmail.com>
2019-01-04 15:27:16 +0100

    fixing index

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function setGradient() {
 	+ ", "
 	+ color2Code
 	+ ")";
+
 	rgba.textContent = "RGBA :" + body.style.background + ";";
 
 	hex.textContent = 
@@ -30,24 +31,24 @@ function setGradient() {
 
 //rgb to hex(6digit)
 function rgb2hex(red, green, blue) {
-    var rgb = blue | (green << 8) | (red << 16);
-    return '#' + (0x1000000 + rgb).toString(16).slice(1)
+	var rgb = blue | (green << 8) | (red << 16);
+	return '#' + (0x1000000 + rgb).toString(16).slice(1)
 }
 
 //hex(6digit) to rgb
 function hex2rgb(hexcode) {
 	var hex = hexcode.slice(0,7);
-    // long version
-    r = hex.match(/^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i);
-    if (r) {
-        return r.slice(1,4).map(function(x) { return parseInt(x, 16); });
-    }
-    // short version
-    r = hex.match(/^#([0-9a-f])([0-9a-f])([0-9a-f])$/i);
-    if (r) {
-            return r.slice(1,4).map(function(x) { return 0x11 * parseInt(x, 16); });
-    }
-    return null;
+	// long version
+	r = hex.match(/^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i);
+	if (r) {
+		return r.slice(1,4).map(function(x) { return parseInt(x, 16); });
+	}
+	// short version
+	r = hex.match(/^#([0-9a-f])([0-9a-f])([0-9a-f])$/i);
+	if (r) {
+		return r.slice(1,4).map(function(x) { return 0x11 * parseInt(x, 16); });
+	}
+	return null;
 }
 
 //convert transparent number from hex(8digit) to rgba 
@@ -62,10 +63,10 @@ function convertOpacity(rgbaPercent) {
 function opacityConvertTable() {
 	var rgbaTable = {};
 	for (var i = 1; i >= 0; i -= 0.01) {
-	  i = Math.round(i * 100) / 100;
-	  var hex = (Math.round(i * 255) + 0x10000).toString(16).substr(-2).toUpperCase();
-	  var perc = Math.round(i * 100);
-	  rgbaTable[perc] = hex;
+		i = Math.round(i * 100) / 100;
+		var hex = (Math.round(i * 255) + 0x10000).toString(16).substr(-2).toUpperCase();
+		var perc = Math.round(i * 100);
+		rgbaTable[perc] = hex;
 	}
 	return rgbaTable;
 }
